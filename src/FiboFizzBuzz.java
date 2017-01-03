c25827bdd02fb5c417c9710a84d91794ad513fb7
Jeremy Pitt <jeremypitt@Jeremys-MacBook-Pro.local>
2017-01-03 13:13:40 -0600

    added custom fib type, refactoring, Joe method

--- src/FiboFizzBuzz.java
+++ src/FiboFizzBuzz.java
@@ -6,14 +6,30 @@ import java.util.Scanner;
 public class FiboFizzBuzz {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int counter = 1;
+        String fiboType = getFiboType(scanner);
         int limit = getLimitCounter(scanner);
-        fibBuzz(counter, limit);
+        switch (fiboType){
+            case "Standard": fibBuzz(limit);
+            case "Custom": customFibBuzz(limit, scanner);
+            default:
+                System.out.println("Error: Invalid type");
+        }
     }
 
-    private static void fibBuzz(int counter, int limit) {
+    private static String getFiboType(Scanner scanner){
+        System.out.println("Which type of Fibonacci would you like to run? [Standard | Custom]");
+        String typeChoice = scanner.nextLine();
+        return typeChoice;
+    }
+
+    private static void fibBuzz(int limit) {
+        int counter = 1;
         int a = 1;
         int b = 1;
+        doFiboMath(limit, counter, a, b);
+    }
+
+    private static void doFiboMath(int limit, int counter, int a, int b) {
         while (counter < limit) {
 
             int c = a + b;
@@ -33,6 +49,21 @@ public class FiboFizzBuzz {
         }
     }
 
+    private static void customFibBuzz(int limit, Scanner scanner){
+        int counter = 1;
+        System.out.println("Enter the first number:");
+        int firstNum = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter the second number:");
+        int secondNum = Integer.parseInt(scanner.nextLine());
+        if (secondNum < firstNum) {
+            int temp = firstNum;
+            firstNum = secondNum;
+            secondNum = temp;
+        }
+
+        doFiboMath(limit, counter, firstNum, secondNum);
+    }
+
     public static int getLimitCounter(Scanner scanner) {
         System.out.println("How deep down the rabbit hole do you wanna go?");
         String cycleString = scanner.nextLine();
@@ -44,5 +75,16 @@ public class FiboFizzBuzz {
             System.exit(0);
         }
         return Integer.parseInt(cycleString);
+    }
+
+    private static void joeFibBuzz(int limit) {
+        int movingVal = 1;
+        int holdingVal = 1;
+        for(int i = 1; i<= limit; i++){
+            int newVal = movingVal + holdingVal;
+            movingVal = holdingVal;
+            holdingVal = newVal;
+            System.out.println(newVal);
+        }
     }
 }
